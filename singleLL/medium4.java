753c47f2fdb95aea8fd07d135305c280affaf865
jathinreddy4 <k.jathinreddy452@gmail.com>
2024-03-25 22:56:03 +0530

    .

---
+++ singleLL/medium4.java
@@ -0,0 +1,50 @@
+
+class Node 
+{
+    int data;
+    Node next;
+    
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class medium4
+{   // function to reverse a LL.
+    public  boolean checkloop(Node head) 
+    {
+        Node fast = head;
+        Node slow = head;
+
+        while(slow != null && fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next;
+
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    } 
+   
+
+    public static void main(String[]args)
+    {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        
+
+        
+
+      
+    }
+}
